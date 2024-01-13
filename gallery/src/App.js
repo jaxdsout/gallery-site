46ec38bd16f3a0fa37607456061eca722d18dd46
jaxdsout <jaxon.southern@mac.com>
2024-01-12 22:44:31 -0600

    prepare to migrate

--- gallery/src/App.js
+++ gallery/src/App.js
@@ -3,8 +3,25 @@ import './App.css';
 function App() {
   return (
     <div>
-    
     </div>
+    /*
+    
+    HEADER
+      -- LOGO
+      -- NAV
+        -- INVENTORY
+        -- EVENTS
+    
+    INVENTORY
+      -- ALL
+      -- SPECIFIC
+      -- BID
+    
+    EVENTS
+
+    FOOTER
+
+    */
   );
 }
 
