6655e4dcfb4525b43c7fdb6c4622e71a2172a580
jaxdsout <jaxon.southern@mac.com>
2024-05-29 20:10:53 -0500

    server issue resolved

--- warehouse/warehouse/settings.py
+++ warehouse/warehouse/settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 load_dotenv()
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -63,10 +64,7 @@ WSGI_APPLICATION = 'warehouse.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600)
 }
 
 
