d0fba321106881ba328d27055949cdcdd0ee884a
jaxdsout <jaxon.southern@mac.com>
2024-05-30 10:41:18 -0500

    auth ports

--- warehouse/warehouse/settings.py
+++ warehouse/warehouse/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['warehouse.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['gallery-warehouse-6e7db4cb0263.herokuapp.com', 'localhost', '127.0.0.1']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
