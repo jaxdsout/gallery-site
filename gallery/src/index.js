ae6695e7fab96cc956997883455bbe402820bf22
jaxdsout <jaxon.southern@mac.com>
2024-06-09 20:08:07 -0500

    hashrouter try

--- gallery/src/index.js
+++ gallery/src/index.js
@@ -5,15 +5,15 @@ import App from './App';
 import 'milligram'
 import 'semantic-ui-css/semantic.min.css';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
-    <React.StrictMode>
+  <React.StrictMode>
+    <HashRouter>
       <App />
-    </React.StrictMode>
-  </Router>
+    </HashRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
